f56ea5cee20ab4f94cc74dccb1899ea3a0f15582
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-02 18:18:44 -0300

    Update Paywall.swift

--- PaywallDemo/Paywall/Paywall.swift
+++ PaywallDemo/Paywall/Paywall.swift
@@ -14,13 +14,24 @@ enum PlanSectionPresentation {
 }
 
 final class Paywall: ObservableObject {
-    let primaryColor: Color
-    let planPresentation: PlanSectionPresentation
+    var primaryColor: Color
+    var planPresentation: PlanSectionPresentation = .progress
+    var primaryHeader: String?
+    var secondaryHeader: String?
+    var actionButtonPrimaryTitle: String
     @Published var options: [Plan] = []
-    @Published var actionButtonPrimaryTitle: String?
     
-    init(primaryColor: Color, planPresentation: PlanSectionPresentation = .progress) {
+    init(
+        primaryColor: Color,
+        planPresentation: PlanSectionPresentation,
+        primaryHeader: String? = nil,
+        secondaryHeader: String? = nil,
+        actionButtonPrimaryTitle: String
+    ) {
         self.primaryColor = primaryColor
         self.planPresentation = planPresentation
+        self.primaryHeader = primaryHeader
+        self.secondaryHeader = secondaryHeader
+        self.actionButtonPrimaryTitle = actionButtonPrimaryTitle
     }
 }

de60b9753a93b8808f39f1c8c2ab2231adf50449
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-02 08:54:58 -0300

    Create Paywall.swift

---
+++ PaywallDemo/Paywall/Paywall.swift
@@ -0,0 +1,14 @@
+//
+//  Paywall.swift
+//  PaywallDemo
+//
+//  Created by Rafael Ramos on 02/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PaywallNew {
+    let options: [Plan]
+    let primaryColor: Color = Color(.red)
+}

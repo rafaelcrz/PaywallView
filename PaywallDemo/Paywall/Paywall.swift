f1a31d70e14186306664b284ebdeab4f4b61551e
Rafael Felipe <rafael.felipe1989@gmail.com>
2023-06-05 08:43:54 -0300

    Update Paywall.swift

--- PaywallDemo/Paywall/Paywall.swift
+++ PaywallDemo/Paywall/Paywall.swift
@@ -8,6 +8,11 @@
 import Foundation
 import SwiftUI
 
+enum FeatureType {
+    case list
+    case carrousel
+}
+
 enum PlanSectionPresentation {
     case expandable
     case progress
@@ -15,21 +20,26 @@ enum PlanSectionPresentation {
 
 final class Paywall: ObservableObject {
     var primaryColor: Color
+    var featureType: FeatureType = .list
     var planPresentation: PlanSectionPresentation = .progress
     var primaryHeader: String?
     var secondaryHeader: String?
     var actionButtonPrimaryTitle: String
-    var cancelType: CancelType = .button
+    var cancelType: CancelType
     @Published var options: [Plan] = []
     
     init(
         primaryColor: Color,
+        cancelType: CancelType = .button,
+        featureType: FeatureType = .list,
         planPresentation: PlanSectionPresentation,
         primaryHeader: String? = nil,
         secondaryHeader: String? = nil,
         actionButtonPrimaryTitle: String
     ) {
         self.primaryColor = primaryColor
+        self.cancelType = cancelType
+        self.featureType = featureType
         self.planPresentation = planPresentation
         self.primaryHeader = primaryHeader
         self.secondaryHeader = secondaryHeader
